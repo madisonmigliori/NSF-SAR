3e16821c6dfacd78e0232b33db43d0761c10971d
madisonmigliori <madisonmigliori@arizona.edu>
2025-06-24 13:14:45 +0300

    updated gettoken.java

---
+++ rag/src/main/java/com/nsf/langchain/ParsingCode/GitToken/GetToken.java
@@ -0,0 +1,24 @@
+package com.nsf.langchain.ParsingCode.GitToken;
+
+import java.io.File;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class GetToken {
+    private String token;
+    
+    public GetToken(){
+        try {
+            Dotenv dotenv = Dotenv.load();
+            String key = dotenv.get("GITHUB_API_TOKEN");
+            this.token = GitHubAppAuth.generateJWT("1393268", new File(key));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getToken(){
+        return this.token;
+    }
+
+}
